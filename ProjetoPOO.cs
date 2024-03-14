c384c07785ce9b61ede9b0bc0033a173b3ef602b
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2024-03-14 15:07:28 -0300

    Update ProjetoPOO.cs

--- ProjetoPOO.cs
+++ ProjetoPOO.cs
@@ -1,169 +1,82 @@
-namespace ProjetoPOO;
-
-class Funcionario
+namespace ProjetoPOO
 {
-    public string Nome { get; set; }
-    public int Matricula { get; set; }
-    public int Cnpj { get; set; }
-    
-    public Funcionario()
+    public class Funcionario
     {
-    }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public string Cnpj { get; set; }
 
-    public Funcionario(string nome, int matricula, int cnpj)
-    {
-        Nome = nome;
-        Matricula = matricula;
-        Cnpj = cnpj;
-    }
-}
-
-class Cidadao
-{
-    public string Cpf { get; set; }
-    public string Nome { get; set; }
-    public int Idade { get; set; }
-    public bool Vacinado { get; set; }
-
-    public Cidadao()
-    {
-        
-    }
-
-    public Cidadao(string cpf, string nome, int idade, bool vacinado)
-    {
-        Cpf = cpf;
-        Nome = nome;
-        Idade = idade;
-        Vacinado = false;
-    }
-    
-}
-
-class CadastroVacinaConsole
-{
-    private List<Funcionario> _funcionarios;
-    private List<Cidadao> _cidadaos;
-
-    public CadastroVacinaConsole()
-    {
-        _funcionarios = new List<Funcionario>();
-        _cidadaos = new List<Cidadao>();
-    }
-
-    public void CadastrarFuncionario()
-    {
-        Console.WriteLine("Cadastro de Funcionário");
-        Console.Write("Seu nome: ");
-        string nome = Console.ReadLine();
-        Console.Write("Matrícula: ");
-        int matricula = Convert.ToInt32(Console.ReadLine());
-        Console.Write("CNPJ da Prestadora de Serviço: ");
-        int cnpj = Convert.ToInt32(Console.ReadLine());
-         
-        _funcionarios.Add(new Funcionario());
-        
-        Funcionario funcionario = new Funcionario();
-        funcionario.Nome = nome;
-        funcionario.Matricula = matricula;
-        funcionario.Cnpj = cnpj;
-        _funcionarios.Add(funcionario);
-
-
-        Console.WriteLine("Funcionário cadastrado com sucesso!\n");
-    }
-
-    public void CadastrarCidadao()
-    {
-        Console.WriteLine("Cadastro de Cidadão");
-        Console.Write("Seu nome: ");
-        string nome = Console.ReadLine();
-        Console.Write("CPF: ");
-        string cpf = Console.ReadLine();
-        Console.Write("Idade: ");
-        int idade = Convert.ToInt32(Console.ReadLine());
-        
-        Cidadao cidadao = new Cidadao(cpf, nome, idade, false);
-
-        _cidadaos.Add(cidadao);
-        
-        cidadao.Cpf = cpf;
-        cidadao.Nome = nome;
-        cidadao.Idade = idade;
-        cidadao.Vacinado = false;
-
-        Console.WriteLine("Cidadão cadastrado com sucesso!\n");
-    }
-
-    public void MarcarVacinado()
-    {
-        Console.WriteLine("Marcar a vacina");
-        Console.Write("CPF do cidadão a ser marcado como vacinado: ");
-        string cpf = Console.ReadLine();
-
-        foreach (Cidadao cidadao in _cidadaos)
+        public Funcionario(string nome, string matricula, string cnpj)
         {
-            if (cidadao.Cpf == cpf)
-            {
-                if (cidadao.Cpf == cpf)
-                {
-                    Console.WriteLine("O cidadão já foi vacinado? (true/false)");
-                    string resposta = Console.ReadLine();
-
-                    if (bool.TryParse(resposta, out bool vacinado))
-                    {
-                        cidadao.Vacinado = vacinado;
-                        Console.WriteLine("Cidadão marcado como vacinado com sucesso!\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Resposta inválida. Utilize 'true' ou 'false'.\n");
-                    }
-
-                    return;
-                }
-            }
+            Nome = nome;
+            Matricula = matricula;
+            Cnpj = cnpj;
         }
-        Console.WriteLine($"Cidadão com CPF {cpf} não encontrado.\n");
     }
 
-    public void Menu()
+    public class Cidadao
     {
-        while (true)
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public bool Vacinado { get; set; }
+
+        public Cidadao(string cpf, string nome, int idade, bool vacinado)
         {
-            Console.WriteLine("Selecione uma opção:");
-            Console.WriteLine("1 - Cadastrar Funcionário");
-            Console.WriteLine("2 - Cadastrar Cidadão");
-            Console.WriteLine("3 - Marcar Cidadão como Vacinado");
-            Console.WriteLine("4 - Sair");
+            Cpf = cpf;
+            Nome = nome;
+            Idade = idade;
+            Vacinado = vacinado;
+        }
+        
+        public void EstaVacinado()
+        {
+            Console.WriteLine("A pessoa está vacinada? (Digite 'sim' ou 'nao'):");
+            string resposta = Console.ReadLine().ToLower();
 
-            int opcao;
-            if (int.TryParse(Console.ReadLine(), out opcao));
-
-            switch (opcao)
+            if (resposta == "sim")
             {
-                case 1:
-                    CadastrarFuncionario();
-                    break;
-                case 2:
-                    CadastrarCidadao();
-                    break;
-                case 3:
-                    MarcarVacinado();
-                    break;
-                case 4:
-                    Console.WriteLine("Tchau!");
-                    return;
-                default:
-                    Console.WriteLine("Opção inválida, por favor tente novamente.\n");
-                    break;
+                Vacinado = true;
+                
+            }
+            else if (resposta == "nao")
+            {
+                Vacinado = false;
+            }
+            else
+            {
+                Console.WriteLine("Resposta inválida. Por favor, digite 'sim' ou 'nao'.");
+                EstaVacinado();
             }
         }
     }
 
-    static void Main()
+    public class CadastroVacina
     {
-        CadastroVacinaConsole cadastro = new CadastroVacinaConsole();
-        cadastro.Menu();
+        List<Funcionario> _funcionarios = new List<Funcionario>();
+        List<Cidadao> _cidadaos = new List<Cidadao>();
+
+        public CadastroVacina()
+        {
+            _funcionarios = new List<Funcionario>();
+            _cidadaos = new List<Cidadao>();
+        }
+
+        public void cadastrarFuncionario()
+        {
+            Console.WriteLine("Cadastro de funcionário");
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Matrícula: ");
+            string matricula = Console.ReadLine();
+            Console.WriteLine("CNPJ: ");
+            string cnpj = Console.ReadLine();
+            
+            _funcionarios.Add(new Funcionario(nome, matricula, cnpj));
+            Console.WriteLine("Cidadão cadastrado com sucesso!");
+
+        }
     }
+    
+    
 }
