53fd14c175eae58b6539c71b3e83cb250cf7b189
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2024-03-22 20:17:19 -0300

    Update ProjetoPOO.cs

--- ProjetoPOO.cs
+++ ProjetoPOO.cs
@@ -165,34 +165,4 @@ class Program
     }
 }
 
-class Cidadao
-{
-    public string Nome { get; }
-    public int Idade { get; }
-    public string CPF { get; }
-    public bool Vaccinated { get; }
 
-    public Cidadao(string nome, string cpf, int idade, bool vaccinated)
-    {
-        Nome = nome;
-        CPF = cpf;
-        Idade = idade;
-        Vaccinated = vaccinated;
-    }
-
-    public Cidadao(string nome, string cpf) : this(nome, cpf, 0, false) { }
-}
-
-class Funcionario
-{
-    public string Nome { get; }
-    public string Matricula { get; }
-    public string CNPJ { get; }
-
-    public Funcionario(string nome, string matricula, string cnpj)
-    {
-        Nome = nome;
-        Matricula = matricula;
-        CNPJ = cnpj;
-    }
-}
